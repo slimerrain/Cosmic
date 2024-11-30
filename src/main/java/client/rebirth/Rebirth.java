f0ce5c63a27eac00d483f2ce9e93b0cfa3f7bbf7
Manon Lefebvre <manonalefebvre@gmail.com>
2024-11-30 01:10:09 -0800

    rebirths w

--- src/main/java/client/rebirth/Rebirth.java
+++ src/main/java/client/rebirth/Rebirth.java
@@ -2,6 +2,16 @@ package client.rebirth;
 
 import client.Character;
 import client.Job;
+import client.Stat;
+import tools.DatabaseConnection;
+import tools.PacketCreator;
+import tools.Pair;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Rebirth {
     public static boolean isEligibleForRebirth (Character chr) {
@@ -27,15 +37,21 @@ public class Rebirth {
     public static void rebirthToJob (Character chr, int jobId) {
         chr.setLevel(1);
         chr.setJob(Job.getById(jobId));
+        addCharacterRebirths(chr);
+        List<Pair<Stat, Integer>> statup = new ArrayList<>(1);
+        statup.add(new Pair<>(Stat.LEVEL, 1));
+        chr.getClient().sendPacket(PacketCreator.updatePlayerStats(statup, true, chr));
+        chr.updateStrDexIntLuk(4,chr.getRebirthCount() * 100);
+    }
 
-        chr.setLuk(4);
-        chr.setInt(4);
-        chr.setDex(4);
-        chr.setStr(4);
-
-        chr.addRebirth();
-        chr.setRemainingAp(chr.getRebirthCount() * 100);
-
-        chr.saveCharToDB();
+    public static void addCharacterRebirths(Character chr) {
+        try (Connection con = DatabaseConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement("UPDATE characters SET rebirths = ? WHERE id = ?")) {
+            ps.setInt(1, chr.getRebirthCount() + 1);
+            ps.setInt(2, chr.getId());
+            ps.executeUpdate();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        }
     }
 }
